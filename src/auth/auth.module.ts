3863b117c9107afc2ca1118c282f7efabbfdec03
jerom ghagra <jeromghagra@gmail.com>
2025-05-19 20:24:23 +0600

    feat: add GoogleStrategy to AuthModule providers

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { JwtStrategy } from './strategies/jwt.strategy';
 import { PassportModule } from '@nestjs/passport';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserSession } from 'src/users/entities/user-session.entity';
+import { JwtStrategy } from './strategies/jwt.strategy';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
-import { UserSession } from 'src/users/entities/user-session.entity';
+import { GoogleStrategy } from './strategies/google.strategy';
 
 @Module({
   imports: [
@@ -25,6 +26,6 @@ import { UserSession } from 'src/users/entities/user-session.entity';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy],
+  providers: [AuthService, JwtStrategy, GoogleStrategy],
 })
 export class AuthModule {}
