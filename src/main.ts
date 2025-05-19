c5ed980e4e7c8ec506bafedca054e78e8f59c660
jerom ghagra <jeromghagra@gmail.com>
2025-05-19 20:12:51 +0600

    feat: add RolesGuard to global guards for enhanced authorization

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { RolesGuard } from './auth/guards/roles.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const reflector = app.get(Reflector);
-  app.useGlobalGuards(new JwtAuthGuard(reflector));
+  app.useGlobalGuards(new JwtAuthGuard(reflector), new RolesGuard(reflector));
 
   await app.listen(process.env.PORT ?? 3000);
 }
